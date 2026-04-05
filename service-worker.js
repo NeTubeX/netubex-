4a281ae863e52f8f5b6f6b46be10ae8acca3a3dd
NeTubeX <273432570+NeTubeX@users.noreply.github.com>
2026-04-05 20:22:59 +0200

    service workers

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,23 @@
-const CACHE_NAME = 'netubex-login-v1';
+const CACHE_NAME = 'netubex-v1';
 const urlsToCache = [
-  '/login.html',
-  '/dashboard.html'
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/pets.html',
+  '/pgc.html',
+  '/pilote.html'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
